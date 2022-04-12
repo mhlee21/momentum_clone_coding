645f67e1a06e9baeaaa84b0cfa91abc176737473
mhlee <mihyun0921@gmail.com>
2022-04-13 00:07:46 +0900

    img 경로 수정

--- js/background.js
+++ js/background.js
@@ -10,6 +10,6 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `/img/${chosenImage}`;
+bgImage.src = `./../img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
